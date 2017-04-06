35106175e8f399c2099a9eba81d3e74cefd74264
Alex19842203 <slavko.alexandr@gmail.com>
2017-04-06 15:30:03 +0300

    initial

--- frontend/dashbord.php
+++ frontend/dashbord.php
@@ -17,3 +17,27 @@ echo "</pre>";
         <button>Confirm</button>
     </p>
 </div>
+<div>
+    <table>
+        <tr>
+            <th width="100">Date</th>
+            <th width="100">Region</th>
+            <th width="200">Subject</th>
+            <th width="150">Author</th>
+            <th width="200">Previous approval</th>
+            <th width="700">Application</th>
+            <th width="200">Status</th>
+            <th width="200">Actions</th>
+        </tr>
+        <tr>
+            <td width="100">22.03.2017</td>
+            <td width="100">East</td>
+            <td width="200">Moving</td>
+            <td width="150">Slavko O. V.</td>
+            <td width="200">Ivanov I. I.</td>
+            <td width="700">&&@@&&&&&&&&&&&&&&&&&``&&&&</td>
+            <td width="200">Work</td>
+            <td width="200">Edit | Delete | View</td>
+        </tr>
+    </table>
+</div>
